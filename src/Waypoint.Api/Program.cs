a4f93d09354e340558fc6a5f77b79ffcd862728f
Chris <chris@casachristo.com>
2026-06-01 00:24:52 -0400

    fix(auth): add missing usings for ClaimActions

--- src/Waypoint.Api/Program.cs
+++ src/Waypoint.Api/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth.Claims;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
